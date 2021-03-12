2e7596671da797a79de450a5cc7a1d491a5993e0
Evgenij_Tobolich <woltet7@gmail.com>
2021-03-12 19:48:23 +0300

    Permission

--- app/src/main/java/tobolich/qr/scanner/MainActivity.kt
+++ app/src/main/java/tobolich/qr/scanner/MainActivity.kt
@@ -1,8 +1,14 @@
 package tobolich.qr.scanner
 
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
 import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.budiyev.android.codescanner.AutoFocusMode
 import com.budiyev.android.codescanner.CodeScanner
 import com.budiyev.android.codescanner.CodeScannerView
@@ -13,12 +19,20 @@ import com.budiyev.android.codescanner.ScanMode
 class MainActivity : AppCompatActivity() {
     private lateinit var codeScanner: CodeScanner
 
+    companion object {
+        private const val REQUEST_CODE_PERMISSIONS = 101
+        private val REQUIRED_PERMISSIONS = arrayOf(Manifest.permission.CAMERA)
+    }
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val scannerView = findViewById<CodeScannerView>(R.id.scanner_view)
 
         codeScanner = CodeScanner(this, scannerView)
+
+        //  codeScanner.startPreview() заупустить камеру
 
         // Parameters (default values)
         codeScanner.camera = CodeScanner.CAMERA_BACK // or CAMERA_FRONT or specific camera id
@@ -28,6 +42,18 @@ class MainActivity : AppCompatActivity() {
         codeScanner.scanMode = ScanMode.SINGLE // or CONTINUOUS or PREVIEW
         codeScanner.isAutoFocusEnabled = true // Whether to enable auto focus or not
         codeScanner.isFlashEnabled = false // Whether to enable flash or not
+
+        when {
+            ContextCompat.checkSelfPermission(
+                    this,
+                    Manifest.permission.CAMERA
+            ) == PackageManager.PERMISSION_GRANTED -> codeScanner.startPreview()
+            else -> ActivityCompat.requestPermissions(
+                    this,
+                    REQUIRED_PERMISSIONS,
+                    REQUEST_CODE_PERMISSIONS
+            )
+        }
 
         // Callbacks
         codeScanner.decodeCallback = DecodeCallback {
@@ -45,7 +71,9 @@ class MainActivity : AppCompatActivity() {
         scannerView.setOnClickListener {
             codeScanner.startPreview()
         }
+
     }
+
 
     override fun onResume() {
         super.onResume()
