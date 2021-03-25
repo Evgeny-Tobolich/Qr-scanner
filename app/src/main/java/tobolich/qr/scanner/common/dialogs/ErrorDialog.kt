9425bc2c0e7c55b0ed9b7dbb0f76a58a7e396221
Evgeny Tobolich <woltet7@gmail.com>
2021-03-25 13:02:21 +0300

    add ErrorDialog

--- app/src/main/java/tobolich/qr/scanner/common/dialogs/ErrorDialog.kt
+++ app/src/main/java/tobolich/qr/scanner/common/dialogs/ErrorDialog.kt
@@ -1,4 +1,30 @@
 package tobolich.qr.scanner.common.dialogs
 
-// TODO: реализовать функционал
-class ErrorDialog
+import android.app.Dialog
+import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
+import androidx.fragment.app.DialogFragment
+import tobolich.qr.scanner.R
+
+class ErrorDialog : DialogFragment() {
+
+    companion object {
+        val TAG = ErrorDialog::class.java.simpleName
+
+        fun newInstance(string: String): ErrorDialog {
+            return ErrorDialog()
+        }
+
+        fun newInstance(throwable: Throwable): ErrorDialog {
+            return ErrorDialog()
+        }
+    }
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog =
+        AlertDialog.Builder(requireContext())
+            .setTitle(getString(R.string.error_dialog_title))
+            .setMessage(getString(R.string.error_dialog_message))
+            .setPositiveButton(getString(R.string.undrstndbly)) { _, _ -> }
+            .create()
+}
+
