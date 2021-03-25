ef1d14cfa115fa3701d85b3e37612251304a3b68
Evgeny Tobolich <woltet7@gmail.com>
2021-03-25 15:04:50 +0300

    add newInstance(string/throwable) + message

--- app/src/main/java/tobolich/qr/scanner/common/dialogs/ErrorDialog.kt
+++ app/src/main/java/tobolich/qr/scanner/common/dialogs/ErrorDialog.kt
@@ -12,19 +12,35 @@ class ErrorDialog : DialogFragment() {
         val TAG = ErrorDialog::class.java.simpleName
 
         fun newInstance(string: String): ErrorDialog {
-            return ErrorDialog()
+            val errorDialog = ErrorDialog()
+            val args = Bundle()
+            args.putString("string", string)
+            errorDialog.arguments = args
+
+            return errorDialog
         }
 
         fun newInstance(throwable: Throwable): ErrorDialog {
-            return ErrorDialog()
+            val errorDialog = ErrorDialog()
+            val args = Bundle()
+            args.putString("throwable", throwable.message)
+            errorDialog.arguments = args
+
+            return errorDialog
         }
     }
 
-    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog =
-        AlertDialog.Builder(requireContext())
-            .setTitle(getString(R.string.error_dialog_title))
-            .setMessage(getString(R.string.error_dialog_message))
-            .setPositiveButton(getString(R.string.undrstndbly)) { _, _ -> }
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        val message = arguments?.getString("string") ?: arguments?.getString("throwable")
+
+        return AlertDialog.Builder(requireContext())
+            .apply {
+                setTitle(getString(R.string.error_dialog_title))
+                setMessage(message)
+                setPositiveButton(getString(R.string.got_it)) { _, _ -> }
+            }
             .create()
+    }
 }
+
 
