d61a34ef605ca488dc2dce50c6faed853d4206d8
Evgeny Tobolich <woltet7@gmail.com>
2021-03-26 09:56:26 +0300

    change ErrorDIalog

--- app/src/main/java/tobolich/qr/scanner/common/dialogs/ErrorDialog.kt
+++ app/src/main/java/tobolich/qr/scanner/common/dialogs/ErrorDialog.kt
@@ -8,36 +8,31 @@ import tobolich.qr.scanner.R
 
 class ErrorDialog : DialogFragment() {
 
+    private val message: String by lazy { "${arguments?.getString(ARG_MESSAGE)}" }
+
     companion object {
         val TAG = ErrorDialog::class.java.simpleName
-        private const val ARG_MESSAGE = "string"
-
-        fun newInstance(string: String): ErrorDialog {
-            val errorDialog = ErrorDialog()
-            val args = Bundle()
-            args.putString(ARG_MESSAGE, string)
-            errorDialog.arguments = args
-
-            return errorDialog
-        }
+        private const val ARG_MESSAGE = "ARG_MESSAGE"
 
         fun newInstance(throwable: Throwable): ErrorDialog {
             return newInstance(throwable.message.toString())
         }
+
+        fun newInstance(message: String): ErrorDialog {
+            return ErrorDialog().apply {
+                arguments = Bundle().apply {
+                    putString(ARG_MESSAGE, message)
+                }
+            }
+        }
     }
 
-    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
-        val message: String by lazy {
-            "${arguments?.getString(ARG_MESSAGE)}"
-        }
-        return AlertDialog.Builder(requireContext())
-            .apply {
-                setTitle(getString(R.string.error_dialog_title))
-                setMessage(message)
-                setPositiveButton(getString(android.R.string.ok)) { _, _ -> }
-            }
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog =
+        AlertDialog.Builder(requireContext())
+            .setTitle(getString(R.string.error_dialog_title))
+            .setMessage(message)
+            .setPositiveButton(getString(android.R.string.ok)) { _, _ -> }
             .create()
-    }
 }
 
 
