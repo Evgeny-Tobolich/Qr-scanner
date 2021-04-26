3fc68d854dcf2dcf986150088824ad081bfb496b
Evgeny Tobolich <woltet7@gmail.com>
2021-04-26 21:10:13 +0300

    add abstract class Interactor

--- app/src/main/java/tobolich/qr/scanner/domain/scanner/ProcessScanResultInteractor.kt
+++ app/src/main/java/tobolich/qr/scanner/domain/scanner/ProcessScanResultInteractor.kt
@@ -1,18 +1,13 @@
 package tobolich.qr.scanner.domain.scanner
 
 import tobolich.qr.scanner.domain.scanner.model.ScanResult
-import tobolich.qr.scanner.domain.scanner.validators.IsPhoneValidator
-import tobolich.qr.scanner.domain.scanner.validators.IsUrlValidator
 
-class ProcessScanResultInteractor {
+class ProcessScanResultInteractor : Interactor() {
 
-    private lateinit var isPhoneValidator: IsPhoneValidator
-    private lateinit var isUrlValidator: IsUrlValidator
-
-    fun execute(input: String): ScanResult = when {
-        isPhoneValidator.isValid(input) -> ScanResult.Phone(input)
-        isUrlValidator.isValid(input) -> ScanResult.Url(input)
-        else -> ScanResult.Text(input)
+    override fun execute(string: String): ScanResult = when {
+        isPhoneValidator.isValid(string) -> ScanResult.Phone(string)
+        isUrlValidator.isValid(string) -> ScanResult.Url(string)
+        else -> ScanResult.Text(string)
     }
 }
 
