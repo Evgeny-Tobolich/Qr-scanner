c11da9c6f62e321ce14860a8ae3aa71ce3267d70
Evgeny Tobolich <woltet7@gmail.com>
2021-04-26 16:01:55 +0300

    delete UseCase.Change Interactor

--- app/src/main/java/tobolich/qr/scanner/domain/scanner/ProcessScanResultInteractor.kt
+++ app/src/main/java/tobolich/qr/scanner/domain/scanner/ProcessScanResultInteractor.kt
@@ -1,12 +1,18 @@
 package tobolich.qr.scanner.domain.scanner
 
 import tobolich.qr.scanner.domain.scanner.model.ScanResult
-import tobolich.qr.scanner.feature.scanner.presentation.ScannerViewModel
+import tobolich.qr.scanner.domain.scanner.validators.IsPhoneValidator
+import tobolich.qr.scanner.domain.scanner.validators.IsUrlValidator
 
-class ProcessScanResultInteractor : UseCase<ScannerViewModel, ScanResult> {
+class ProcessScanResultInteractor {
 
-    override fun execute(input: ScannerViewModel): ScanResult {
-       //TODO()
+    private lateinit var isPhoneValidator: IsPhoneValidator
+    private lateinit var isUrlValidator: IsUrlValidator
 
+    fun execute(input: String): ScanResult = when {
+        isPhoneValidator.isValid(input) -> ScanResult.Phone(input)
+        isUrlValidator.isValid(input) -> ScanResult.Url(input)
+        else -> ScanResult.Text(input)
     }
 }
+
--- app/src/main/java/tobolich/qr/scanner/domain/scanner/UseCase.kt
+++
@@ -1,6 +0,0 @@
-package tobolich.qr.scanner.domain.scanner
-
-interface UseCase<in ScannerViewModel, out ScanResult> {
-
-    fun execute(input: ScannerViewModel): ScanResult
-}
