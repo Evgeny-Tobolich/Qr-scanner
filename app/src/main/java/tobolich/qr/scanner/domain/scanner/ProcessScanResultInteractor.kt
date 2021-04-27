a9620f2635a9dbd886b069dda070bf59c55f15b1
Evgeny Tobolich <woltet7@gmail.com>
2021-04-27 11:00:19 +0300

    add in constructor. Delete Interactor class

--- app/src/main/java/tobolich/qr/scanner/domain/scanner/ProcessScanResultInteractor.kt
+++ app/src/main/java/tobolich/qr/scanner/domain/scanner/ProcessScanResultInteractor.kt
@@ -1,10 +1,15 @@
 package tobolich.qr.scanner.domain.scanner
 
 import tobolich.qr.scanner.domain.scanner.model.ScanResult
+import tobolich.qr.scanner.domain.scanner.validators.IsPhoneValidator
+import tobolich.qr.scanner.domain.scanner.validators.IsUrlValidator
 
-class ProcessScanResultInteractor : Interactor() {
+class ProcessScanResultInteractor(
+    private val isPhoneValidator: IsPhoneValidator,
+    private val isUrlValidator: IsUrlValidator
+) {
 
-    override fun execute(string: String): ScanResult = when {
+    fun execute(string: String): ScanResult = when {
         isPhoneValidator.isValid(string) -> ScanResult.Phone(string)
         isUrlValidator.isValid(string) -> ScanResult.Url(string)
         else -> ScanResult.Text(string)
